ab4b5cde73bdada1cf66cb1642dbee7b79dc0d76
Richard Fuentes <richard10d@gmail.com>
2025-04-10 19:20:54 -0500

    cambio 2

--- SEMANA3JUEVES/src/semana3jueves/V1.java
+++ SEMANA3JUEVES/src/semana3jueves/V1.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class V1 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -37,6 +41,18 @@ public class V1 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nombre :");
+		lblNewLabel.setBounds(30, 42, 49, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(82, 40, 160, 17);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(264, 38, 89, 23);
+		contentPane.add(btnNewButton);
 	}
-
 }
